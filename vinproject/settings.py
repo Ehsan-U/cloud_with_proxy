c94815748968c2bddf3a7b5133ba3c0941d1d92b
python-cinnamon <88642086+python-cinnamon@users.noreply.github.com>
2022-01-09 18:21:47 +0500

    delay_added

--- vinproject/settings.py
+++ vinproject/settings.py
@@ -25,7 +25,7 @@ NEWSPIDER_MODULE = 'vinproject.spiders'
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 2
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,7 +64,7 @@ DOWNLOADER_MIDDLEWARES = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'vinproject.pipelines.ExcelWriter': 1,
+   'vinproject.pipelines.MongoCloud': 1,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
